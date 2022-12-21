d1739ff335750c64d35c01a3696c9b623fdbf209
Sangrok Han <zibets.han@gmail.com>
2022-12-22 01:03:51 +0900

    Initial Spring files
    Signed-off-by: Sangrok Han <zibets.han@gmail.com>

---
+++ src/main/java/com/oddbie/wannyback/WannyBackApplication.java
@@ -0,0 +1,13 @@
+package com.oddbie.wannyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WannyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WannyBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oddbie/wannyback/WannyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oddbie.wannyback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WannyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
