5ec6465076bf3ea93665e49ea71aa143c124a7e1
Sangrok Han <zibets.han@gmail.com>
2022-12-26 22:55:20 +0900

    Hello Response on spring
    Signed-off-by: Sangrok Han <zibets.han@gmail.com>

---
+++ src/main/java/com/oddbie/wannyback/HelloController.java
@@ -0,0 +1,12 @@
+package com.oddbie.wannyback;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/api/hello")
+    public String Hello() {
+        return "Hello from String\n";
+    }
+}
