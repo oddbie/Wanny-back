28a3105f246ac461106b67e00e80d250beb75517
Sangrok Han <zibets.han@gmail.com>
2023-01-08 16:45:57 +0900

    Apply Jasypt
    Signed-off-by: Sangrok Han <zibets.han@gmail.com>

---
+++ src/main/java/com/oddbie/wannyback/JasyptConfig.java
@@ -0,0 +1,31 @@
+package com.oddbie.wannyback;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.beans.factory.annotation.Value;
+
+@Configuration
+@PropertySource("application.properties")
+public class JasyptConfig {
+
+    @Value("${jasypt.encryptor.password}")
+    private String password;
+
+    @Bean(name = "jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(password);
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
--- src/main/java/com/oddbie/wannyback/todo/repository/TodoRepository.java
+++ src/main/java/com/oddbie/wannyback/todo/repository/TodoRepository.java
@@ -2,15 +2,13 @@ package com.oddbie.wannyback.todo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import com.oddbie.wannyback.todo.model.Todo;
 
-// @Repository
-@EnableJpaRepositories
+@Repository
 public interface TodoRepository extends JpaRepository<Todo, Integer> {
-    // @Query("SELECT u FROM Todo u")
-    // public List<Todo> findAll();
+    @Query("SELECT u FROM Todo u")
+    public List<Todo> findAll();
 }
